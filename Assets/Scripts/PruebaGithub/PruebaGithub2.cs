1633d2497f296c07b2f3335eb6a3130705289914
MaruFelis <amaruse123@gmail.com>
2024-01-24 16:06:29 -0300

    mi primer commit

---
+++ Assets/Scripts/PruebaGithub/PruebaGithub2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaGithub2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Es un mensaje de un objeto");
+        Debug.Log("Se agrego a felix con un diamante");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
